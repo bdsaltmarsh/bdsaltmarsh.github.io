60a567389f417d091d8fa8beeab36fe812af9266
Zach Neuman <zach@xinu.io>
2020-07-28 16:02:06 -0500

    New Post

--- en.search.min.9c6fd043e86f8d9e71709eec12f3e4135dbe811b3e0524f2d1e9bbae835dc8b2.js
+++ en.search.min.9c6fd043e86f8d9e71709eec12f3e4135dbe811b3e0524f2d1e9bbae835dc8b2.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.4c10827582e81e6d267eb12d13e3fb0c7e9ded539a666e5044e1875907f1e0ce.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.fadce4e600b570fc2aa5511b2557fdb215f3a36cad0a962433ce12292c48a295.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
