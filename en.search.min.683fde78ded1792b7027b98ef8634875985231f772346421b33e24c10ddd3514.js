1cb69b05d4d7d0c3105fb44220a286bb21393682
Zach Neuman <zach@xinu.io>
2020-07-28 21:35:37 -0500

    Adding Saltmarsh Map

--- en.search.min.683fde78ded1792b7027b98ef8634875985231f772346421b33e24c10ddd3514.js
+++ en.search.min.683fde78ded1792b7027b98ef8634875985231f772346421b33e24c10ddd3514.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.fadce4e600b570fc2aa5511b2557fdb215f3a36cad0a962433ce12292c48a295.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.17eec8392b0409a7dbdf189bec8426e6c6fe4c4bce3595397e57097f09b405af.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
