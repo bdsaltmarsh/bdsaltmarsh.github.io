14845ae0f9b189c1a88403748f91fe26fd255b63
Zach Neuman <zach@xinu.io>
2020-08-07 13:25:01 -0500

    Adding quests.md to site.

--- en.search.min.87f4a058ec131b456cd1644b6a0afdf942c4f9dc57fa67a78b15b41baed3d9b8.js
+++ en.search.min.87f4a058ec131b456cd1644b6a0afdf942c4f9dc57fa67a78b15b41baed3d9b8.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.de54d9d4a309869327517d4ec954c7be9bfda9f8d28defcdb2063d4ed138384f.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.c5b64abde79f305d15922b83145da0ac8a188b864b20739b93c4f22c4ac8eae4.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
