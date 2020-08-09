c25398bcbd7b0b08874899a998da9ce5c2275a94
Zach Neuman <zach@xinu.io>
2020-08-09 17:36:59 -0500

    fixing gold ledger

--- en.search.min.a7a43029283b3fbc1a313d11cd2bcfd9bbc7df55faf1506396b7e1f6b8473cc9.js
+++ en.search.min.a7a43029283b3fbc1a313d11cd2bcfd9bbc7df55faf1506396b7e1f6b8473cc9.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.46f4ccd0207e25ef6d01ac101b1227186003032e1ce7d9e20314644d7bf052f5.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.59cf00ba53a6eb827665ae711a9ac7d90a29c194d42a08c58cc169d45fe919b1.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
