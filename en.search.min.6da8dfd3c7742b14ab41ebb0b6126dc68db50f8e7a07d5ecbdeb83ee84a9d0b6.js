e7b1c8f04fe573f5181335a7d24c49a3360327d2
Zach Neuman <zach@xinu.io>
2020-08-09 17:25:33 -0500

    Updating gold and rough draft of session info.

--- en.search.min.6da8dfd3c7742b14ab41ebb0b6126dc68db50f8e7a07d5ecbdeb83ee84a9d0b6.js
+++ en.search.min.6da8dfd3c7742b14ab41ebb0b6126dc68db50f8e7a07d5ecbdeb83ee84a9d0b6.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.5e3b829e343962afeb6904169ab4d662e7e672ba82a05a38a003c35305a7dbf8.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.46f4ccd0207e25ef6d01ac101b1227186003032e1ce7d9e20314644d7bf052f5.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
