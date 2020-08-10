346ddf972fc66b5b7afb707778de4cecec5a76e2
Zach Neuman <zach@xinu.io>
2020-08-09 21:11:20 -0500

    Updating Varanus' Library

--- en.search.min.2615bc015d4b89baff561614e70fbcf904001210340d4afc0b8df6e1026d039e.js
+++ en.search.min.2615bc015d4b89baff561614e70fbcf904001210340d4afc0b8df6e1026d039e.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.9c552071c04a94ad90d3506c5a01d31a263cd92fefa29b9e0c663c9bc98fe904.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.fb9f188b568cc555d14afc38356e769b60f1c86b21100e73ffd985290560956e.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
